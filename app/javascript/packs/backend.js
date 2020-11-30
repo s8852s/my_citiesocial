337a02a72cdcf3dc04a37c58b1c3b407fba87eec
s8852s <s8852s@hotmail.com.tw>
2020-12-01 01:13:36 +0800

    frontend navbar footer carousel

--- app/javascript/packs/backend.js
+++ app/javascript/packs/backend.js
@@ -12,13 +12,13 @@ require("@rails/activestorage").start()
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-const images = require.context('../images', true)
-const imagePath = (name) => images(name, true)
+// const images = require.context('../images', true)
+// const imagePath = (name) => images(name, true)
 
 import "controllers";
 
 import "scripts/shared";
-import "scripts/frontend";
+import "scripts/backend";
 
 import "styles/shared";
-import "styles/frontend";
+import "styles/backend";

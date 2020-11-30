dc949f7aba2f70ad11a0e5f36feaecac3d25cc02
s8852s <s8852s@hotmail.com.tw>
2020-11-30 18:49:03 +0800

    install bulma font_awesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,5 @@ require("@rails/activestorage").start()
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "controllers"
+import "controllers";
+import "styles/frontend";

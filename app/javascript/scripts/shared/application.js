f5b0e8e1131826c5a720034216ef7322eea7d54b
s8852s <s8852s@hotmail.com.tw>
2020-12-02 18:51:21 +0800

    install devise sign_in page flash

--- app/javascript/scripts/shared/application.js
+++ app/javascript/scripts/shared/application.js
@@ -24,3 +24,14 @@ document.addEventListener('turbolinks:load', () => {
   }
 
 });
+
+
+document.addEventListener('turbolinks:load', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    var $notification = $delete.parentNode;
+
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});

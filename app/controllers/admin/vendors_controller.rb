8f3496e9fdf50f11800ab0918e10e088f4f40e97
s8852s <s8852s@hotmail.com.tw>
2020-12-17 02:30:12 +0800

    admin/vendor crud

---
+++ app/controllers/admin/vendors_controller.rb
@@ -0,0 +1,48 @@
+class Admin::VendorsController < Admin::BaseController
+  before_action :find_vendor, only: [:edit, :update, :destroy]
+
+  def index
+    @vendors = Vendor.all
+  end
+
+  def new
+    @vendor = Vendor.new
+  end
+
+  def create
+    @vendor = Vendor.new(vendor_params)
+
+    if @vendor.save
+      redirect_to admin_vendors_path, notice: "新增廠商成功"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+
+    if @vendor.update(vendor_params)
+      redirect_to edit_admin_vendor_path(@vendor), notice: "資料更新成功"
+    else
+      render :edit
+    end
+
+    def destroy
+      @vendor.destroy if @vendor
+      redirect_to admin_vendors_path, notice: "刪除廠商成功"
+    end
+  end
+
+  private
+  def find_vendor
+    @vendor = Vendor.find_by(id: params[:id])
+  end
+
+  
+  def vendor_params
+      params.require(:vendor).permit(:title, :description, :online)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root "products#index"
     resources :products
+    resources :vendors, expect: [:show]
   end
   
   

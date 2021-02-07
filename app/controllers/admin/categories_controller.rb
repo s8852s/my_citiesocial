0a5a75eb425ff96b4a097d05de18b69f91311107
s8852s <s8852s@hotmail.com.tw>
2021-02-07 22:26:53 +0800

    category CRUD

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,8 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
-    @categories = Category.all
+    @categories = Category.all.order(position: :asc)
   end
 
   def new
@@ -16,8 +18,28 @@ class Admin::CategoriesController < Admin::BaseController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to admin_categories_path, notice: '編輯分類成功'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy if @category
+    redirect_to admin_categories_path, notice: '分類已成功'
+  end
+
   private
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
   end
 end

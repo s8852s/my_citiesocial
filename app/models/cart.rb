b74c6506b0f27e8d8e20bae0c603e896daf5dff9
s8852s <s8852s@hotmail.com.tw>
2021-02-16 22:20:28 +0800

    購物車Rspec 基本功能2.加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,10 +1,16 @@
 #  PORO = Plain Old Ruby Object 純Ruby類別
 class Cart
+  attr_reader :items
   def initialize
     @items = []
   end
-  def add_item(product_id)
-    @items << product_id
+  def add_item(product_id, quantity = 1)
+    found = @items.find { |item| item.product_id == product_id }
+    if found
+      found.increment!
+    else
+      @items << CartItem.new(product_id, quantity = 1)
+    end
   end
 
   def empty?

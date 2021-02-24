0b767a02c864c29e4b95a1b3c7675a98ba8e26ce
s8852s <s8852s@hotmail.com.tw>
2021-02-24 22:33:55 +0800

    購物車Rspec進階功能2.也可以存放在 Session 的內容（Hash 格式），還原成購物車的內容。

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,8 @@
 #  PORO = Plain Old Ruby Object 純Ruby類別
 class Cart
   attr_reader :items
-  def initialize
-    @items = []
+  def initialize(items = [])
+    @items = items
   end
 
   def serialize
@@ -15,6 +15,24 @@ class Cart
                                   "quantity" => item.quantity } }
 
     { "items" => items }
+  end
+
+  def self.from_hash(hash = nil)
+    # {
+    #   "items" => [
+    #     {"product_id" => 1, "quantity" => 3},
+    #     {"product_id" => 2, "quantity" => 2},
+    #   ]
+    # }
+
+    if hash && hash["items"]
+      items = hash["items"].map { | item |
+        CartItem.new(item["product_id"], item["quantity"])
+      }
+      Cart.new(items)
+    else
+      Cart.new
+    end
   end
 
   def add_item(product_id, quantity = 1)

90626de064b0181a0bfd2fd72bdd10922edd6034
s8852s <s8852s@hotmail.com.tw>
2021-02-26 02:19:00 +0800

    購物車Rspec code整理

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
+
+  let(:cart) { Cart.new }
+
   context "基本功能" do
     it "可以把商品丟到到購物車裡，然後購物車裡就有東西了。" do
-      cart = Cart.new
       cart.add_item(2) # 購物車裡放進2號商品
       expect(cart.empty?).to be false
     end
 
     it "加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變。" do
-      cart = Cart.new
       3.times { cart.add_item(1) }
       2.times { cart.add_item(2) }
       expect(cart.items.count).to be 2
@@ -17,7 +18,6 @@ RSpec.describe Cart, type: :model do
     end
 
     it "商品可以放到購物車裡，也可以再拿出來。" do
-      cart = Cart.new
       # v1 = Vendor.create(title: "v1")
       # p1 = Product.create(name: 'kk', list_price: 10, sell_price: 5, vendor: v1)
       p1 = FactoryBot.create(:product)
@@ -27,7 +27,6 @@ RSpec.describe Cart, type: :model do
   end
 
   it "可以計算整台購物車的總消費金額。" do 
-    cart = Cart.new
     p1 = FactoryBot.create(:product, sell_price: 5)
     p2 = FactoryBot.create(:product, sell_price: 10)
 
@@ -39,7 +38,6 @@ RSpec.describe Cart, type: :model do
   end
 
   it "特別活動可搭配折扣（例如聖誕節的時候全面打 9 折，或是滿額滿千送百或滿額免運費）。" do
-    cart = Cart.new
     p1 = FactoryBot.create(:product, sell_price: 50)
     p2 = FactoryBot.create(:product, sell_price: 100)
 
@@ -53,7 +51,6 @@ RSpec.describe Cart, type: :model do
 
   context "進階功能" do
     it "可以將購物車內容轉換成 Hash 並存到 Session 裡。" do
-      cart = Cart.new
     p1 = FactoryBot.create(:product)
     p2 = FactoryBot.create(:product)
 
@@ -80,18 +77,4 @@ RSpec.describe Cart, type: :model do
       }
     end
   end
-
-
-# 可以把商品丟到到購物車裡，然後購物車裡就有東西了。
-# 如果加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變。
-# 商品可以放到購物車裡，也可以再拿出來。
-# 每個 Cart Item 都可以計算它自己的金額（小計）。
-# 可以計算整台購物車的總消費金額。
-# 特別活動可搭配折扣（例如聖誕節的時候全面打 9 折，或是滿額滿千送百或滿額免運費）。
-
-# 進階功能
-# 因為購物車將會以 Session 方式儲存，所以：
-
-# 可以將購物車內容轉換成 Hash 並存到 Session 裡。
-# 也可以存放在 Session 的內容（Hash 格式），還原成購物車的內容。
 end

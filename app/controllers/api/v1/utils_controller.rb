82c524b08963a4bd830a862180acaf789083786b
s8852s <s8852s@hotmail.com.tw>
2021-03-01 05:16:05 +0800

    購物車Api utils#cart

--- app/controllers/api/v1/utils_controller.rb
+++ app/controllers/api/v1/utils_controller.rb
@@ -8,4 +8,16 @@ class Api::V1::UtilsController < ApplicationController
       render json: { status: 'duplicated', email: email}
     end
   end
+
+  def cart
+    product = Product.friendly.find(params[:id])
+
+    if product
+      cart = Cart.from_hash(session[:my_cart])
+      cart.add_item(product.code)
+      session[:my_cart] = cart.serialize
+      render json: { status: "OK", items: cart.items.count }
+    end
+    
+  end
 end
--- app/javascript/controllers/product_controller.js
+++ app/javascript/controllers/product_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from 'stimulus'
+import Rails from "@rails/ujs";
 
 export default class extends Controller {
-  static targets = [ "quantity" ]
+  static targets = [ "quantity", "sku", "addToCartButton" ]
 
   connect(){
   }
@@ -20,5 +21,32 @@ export default class extends Controller {
     let q = Number(this.quantityTarget.value);
     this.quantityTarget.value = q + 1;
     console.log("+");
+  };
+
+  add_to_cart(evt){
+    evt.preventDefault();
+    let product_id = this.data.get("id");
+    let quantity = this.quantityTarget.value;
+    let sku = this.skuTarget.value;
+
+    if (quantity > 0) {
+      this.addToCartButtonTarget.classList.add('is-loading');
+      let data = new FormData();
+      data.append("id", product_id);
+      data.append("quantity", quantity);
+      data.append("sku", sku);
+
+      Rails.ajax({
+        url: "/api/v1/cart",
+        type: "POST",
+        data: data,
+        success: resp => {
+          console.log(resp);
+        },
+        error: err => {
+          console.log(err);
+        }
+      });
+    };
   };
 };

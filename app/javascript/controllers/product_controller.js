92bf35409f4133312f4d8acada4c85483cad7b2d
s8852s <s8852s@hotmail.com.tw>
2021-03-01 04:54:45 +0800

    商品頁面數量可以增加減少

---
+++ app/javascript/controllers/product_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  static targets = [ "quantity" ]
+
+  connect(){
+  }
+
+  quantity_minus(evt){
+    evt.preventDefault();
+    let q = Number(this.quantityTarget.value);
+    if ( q > 1 ){
+      this.quantityTarget.value = q - 1;
+    }
+    console.log("-");
+  };
+
+  quantity_plus(evt){
+    evt.preventDefault();
+    let q = Number(this.quantityTarget.value);
+    this.quantityTarget.value = q + 1;
+    console.log("+");
+  };
+};

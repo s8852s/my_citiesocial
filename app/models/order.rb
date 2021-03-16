3608152b68743dcce29ff56f7868019188d139f6
s8852s <s8852s@hotmail.com.tw>
2021-03-16 23:28:45 +0800

    gem install faraday 訂單建立前產生訂單編號

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
+
   validates :recipient, :tel, :address, presence: true
+
+  before_create :generate_order_num
+  private
+  def generate_order_num
+    self.num = SecureRandom.hex(5) unless num
+  end
 end
